69abc806fa5a59cff64823f3e9dd15bce0304757
robinsamvarghese <robinsamvarghese990@gmail.com>
2024-06-07 18:54:32 +0530

    third commit made for the webpage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 function togglemenu(){
-    const manu= document.querySelector(".menu-links");
-    const icon= document.querySelector(".hambuge-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-
+    const menu =document.querySelector(".menu-links");
+    const icon =document.querySelector(".hamburger-icon");
+     menu.classList.toggle("open");
+     icon.classList.toggle("open");
 }
